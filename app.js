4ccafe7d43fb7ad2deb4a1be5cfa44d3d8867e0b
nourin bushair <nourinbusahir@gmail.com>
2026-09-24 20:07:22 +0530

    Initial company expense app

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-const SUPABASE_URL = "https://kdabddhuxypcihlbubzi.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://kdabddhuxypcihlbubzi.supabase.co";
 const SUPABASE_KEY = "sb_publishable_nkJMnNS4PSIQlNfGvXh7Gg_k9C7X9TA";
 
 const supabaseClient = window.supabase.createClient(
